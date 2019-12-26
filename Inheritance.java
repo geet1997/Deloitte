c19ff0ab609dbb0f85101ce651103b342cab1bf4
geet1997 <59157233+geet1997@users.noreply.github.com>
2019-12-26 10:14:04 +0530

    Add files via upload

---
+++ Inheritance.java
@@ -0,0 +1,16 @@
+package geet;
+
+public class Inheritance {
+	
+		public static void main(String[] args) {
+			Animal d = new Lion();
+			d.eat();
+			d.sleep();
+			d.makenoise();
+			 d = new Tiger();
+			d.eat();
+			d.sleep();
+			d.makenoise();
+		}
+	
+}
